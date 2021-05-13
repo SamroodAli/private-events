1a38c9cc1f603de084ed2c0cf6a2c863519dbbe0
samroodAli <devsamrood@gmail.com>
2021-05-13 19:22:34 +0530

    Feat:Add guard auto test gem

--- spec/hello_world_spec.rb
+++ spec/hello_world_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'Hello world', type: :system do
   describe 'index page' do
     it 'shows the right content' do
       visit root_path
-      expect(page).to have_content('hello world!')
+      expect(page).to have_content('hello world')
     end
   end
 end
