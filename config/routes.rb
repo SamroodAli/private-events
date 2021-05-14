54050abd7a6ceedfd23b550006cc4ad9114f85de
samroodAli <devsamrood@gmail.com>
2021-05-14 19:29:46 +0530

    Feat:Add all events in users show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'static_pages/home'
   devise_for :users, controllers: { sessions: 'users/sessions' ,registrations: 'users/registrations'}
-  root "static_pages#home"
   resources :events
   resources :tickets
+  resources :users, only: :show
+  root "static_pages#home"
 end
