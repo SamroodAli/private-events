5b888be5c8c60ada96d0a9f07e93fda500900a1a
samroodAli <devsamrood@gmail.com>
2021-05-14 15:50:55 +0530

    Event:Remove user from events table

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,8 +2,6 @@ class EventsController < ApplicationController
   def index
     if signed_in?
       @events = Event.where(user_id: current_user.id)
-    else
-      @events = Event.all
     end
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_14_084447) do
+ActiveRecord::Schema.define(version: 2021_05_14_101257) do
 
   create_table "events", force: :cascade do |t|
     t.datetime "date"
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
