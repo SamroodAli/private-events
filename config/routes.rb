8c1001f94e995c4865b38cd74760bf574b6eedb1
samroodAli <devsamrood@gmail.com>
2021-05-14 15:14:01 +0530

    Bootstrap:Add header navbar using bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get 'static_pages/home'
   devise_for :users, controllers: { sessions: 'users/sessions' ,registrations: 'users/registrations'}
   root "static_pages#home"
+  resources :events
 end
