b7ccb5240ca4395fef82814048ad129d4ebf4104
samroodAli <devsamrood@gmail.com>
2021-05-14 14:08:17 +0530

    Model:Add and migrate event model with date:datetime and description:text

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_14_044633) do
+ActiveRecord::Schema.define(version: 2021_05_14_083740) do
+
+  create_table "events", force: :cascade do |t|
+    t.datetime "date"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
