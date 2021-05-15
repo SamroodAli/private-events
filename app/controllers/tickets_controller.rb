d18e59ebce8f897e2cda49bd35dd126eb0c4d2d9
samroodAli <devsamrood@gmail.com>
2021-05-15 22:22:02 +0530

    Fix:Fix already attending events

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,12 +1,15 @@
 class TicketsController < ApplicationController
   def create
     event = Event.find_by(id: params[:id])
-    unless event.attendees.include?(current_user)
+    if !event
+      flash[:warning] = "No such event"
+      redirect_to root_path
+    elsif !event.attendees.include?(current_user)
       Ticket.create(event_id: params[:id], attendee_id: current_user.id)
       event = Event.find_by(id: params[:id])
       redirect_to event
     else
-      flash[:alert] = "You already are attending"
+      flash[:info ] = "You already are attending"
       redirect_to event
     end
   end
