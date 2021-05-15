f710b8806e10f6ec5e591c7919c56a309b2155b7
samroodAli <devsamrood@gmail.com>
2021-05-15 21:55:04 +0530

    Migration:Remove migration files for event attendees

--- db/migrate/20210514103056_add_columns_to_event_attendees.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnsToEventAttendees < ActiveRecord::Migration[6.1]
-  def change
-    add_column :event_attendees, :event_id, :integer
-    add_column :event_attendees, :attendee_id, :integer
-  end
-end
--- db/migrate/20210514112220_drop_event_attendees.rb
+++
@@ -1,9 +0,0 @@
-class DropEventAttendees < ActiveRecord::Migration[6.1]
-  def up
-    drop_table :event_attendees
-  end
-
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
-end
