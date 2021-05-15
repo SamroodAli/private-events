0f4ed3683f4f45c8d8f040d5386b85ec47c3c343
samroodAli <devsamrood@gmail.com>
2021-05-15 23:48:00 +0530

    Refactor:Refactor future and past events using helpers

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,12 @@
 module UsersHelper
+
+  def my_future_events
+    current_user.attended_events.upcoming
+  end
+  def my_past_events
+    current_user.attended_events.past
+  end
+  def my_future_events
+    current_user.attended_events.upcoming
+  end
 end
