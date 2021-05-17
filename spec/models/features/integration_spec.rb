c833a5077b45fe18f34ece356d583ff7313c9d49
samroodAli <devsamrood@gmail.com>
2021-05-17 15:34:39 +0530

    Test:Move sign in to before each

--- spec/models/features/integration_spec.rb
+++ spec/models/features/integration_spec.rb
@@ -3,6 +3,12 @@ require 'rails_helper'
 RSpec.describe 'Events Page', type: :system do
   fixtures :users
 
+  before(:each) do
+      visit 'users/sign_in'
+      fill_in :user_email,with: users(:samrood).email
+      fill_in :user_password,with: :password
+  end
+
   describe 'Events page' do
     it 'shows past andupcoming events' do
       visit 'users/sign_in'
@@ -13,5 +19,9 @@ RSpec.describe 'Events Page', type: :system do
       expect(page).to have_content('Upcoming events')
       expect(page).to have_content('Past events')
     end
+  end
+
+  describe 'Users page' do
+    it
   end
 end
