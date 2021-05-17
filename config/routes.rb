b23f26893d21532497cd69c159ba9bbcaaecdbaf
samroodAli <devsamrood@gmail.com>
2021-05-17 16:29:45 +0530

    Fix:Routes limit to only fit the needs

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions' ,registrations: 'users/registrations'}
-  resources :events
-  resources :tickets
+  resources :events, only: %i[index new create show]
+  resources :tickets, only: %i[create]
   resources :users, only: :show
   delete '/tickets', to: "tickets#destroy"
   root "static_pages#home"
