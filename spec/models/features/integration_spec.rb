84dafb49d6926c709307609acff16185bd05afbd
samroodAli <devsamrood@gmail.com>
2021-05-17 15:54:07 +0530

    Feat:Add users page test

--- spec/models/features/integration_spec.rb
+++ spec/models/features/integration_spec.rb
@@ -2,15 +2,9 @@ require 'rails_helper'
 
 RSpec.describe 'Events Page', type: :system do
   fixtures :users
-
-  before(:each) do
-      visit 'users/sign_in'
-      fill_in :user_email,with: users(:samrood).email
-      fill_in :user_password,with: :password
-  end
-
-  describe 'Events page' do
-    it 'shows past andupcoming events' do
+    
+    describe 'Events page' do
+      it 'shows past andupcoming events' do
       visit 'users/sign_in'
       fill_in :user_email,with: users(:samrood).email
       fill_in :user_password,with: :password
@@ -22,6 +16,15 @@ RSpec.describe 'Events Page', type: :system do
   end
 
   describe 'Users page' do
-    it
+    it 'should have upcoming and past events' do
+      visit 'users/sign_in'
+      fill_in :user_email,with: users(:samrood).email
+      fill_in :user_password,with: :password
+      click_button 'Log in'
+      visit 'users/1'
+      expect(page).to have_content 'Past event'
+      expect(page).to have_content 'Present event'
+      expect(page).to have_content 'Future event'
+    end
   end
 end
